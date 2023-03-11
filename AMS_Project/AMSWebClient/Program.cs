e22006791ebd14a2dbbfed928b4a9b1eed153eff
Thanh Tra <82217333+meofiscoding@users.noreply.github.com>
2023-03-11 14:19:01 +0700

    feat: Add styled homepage with hero section and features section
    - Add hero section with background image
    - Add features section with icons
    - Use Font Awesome for icons
    - Update layout with Font Awesome CDN
    
    The new hero background image replaces the old one and adds a more vibrant and lively feel to the homepage. The features section with icons adds visual interest and makes it easy for students to see the key features of the learning platform. Font Awesome is used to provide high-quality icons.

--- AMS_Project/AMSWebClient/Program.cs
+++ AMS_Project/AMSWebClient/Program.cs
@@ -7,14 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
