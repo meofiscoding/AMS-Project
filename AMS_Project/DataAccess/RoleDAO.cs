9172cad9d4d061de29d10d7d4e8fb3c76e385a91
Thanh Tra <82217333+meofiscoding@users.noreply.github.com>
2023-03-13 23:45:34 +0700

    refactor: update DataAccessclass library project reference
    The DataAccess class library was previously referencing an outdated BusinessObject library. We have updated the reference to point to a new BusinessObject library that we have created.

--- AMS_Project/DataAccess/RoleDAO.cs
+++ AMS_Project/DataAccess/RoleDAO.cs
@@ -1,4 +1,4 @@
-﻿using BussinessObject.DataAccess;
+﻿using BusinessObject.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- AMS_Project/DataAccess/UserDAO.cs
+++ AMS_Project/DataAccess/UserDAO.cs
@@ -1,4 +1,4 @@
-﻿using BussinessObject.DataAccess;
+﻿using BusinessObject.DataAccess;
 
 namespace DataAccess
 {
